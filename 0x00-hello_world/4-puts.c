fbf884935fa9f3c4c32018769bd12509c53b8819
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-05 16:52:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point that display information using the puts function.
+ *
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
