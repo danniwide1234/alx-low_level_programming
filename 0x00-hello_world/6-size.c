c1d0297f5048d02b8a4a9477e88626017f958931
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-05 18:18:58 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - An entry point C program that prints the size of
+ * various types on the computer.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	char z;
+	int y;
+	long int p;
+	long long int x;
+	float k;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)
+sizeof(z));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)
+sizeof(y));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)
+sizeof(p));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
+sizeof(x));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)
+sizeof(k));
+return (0);
+}
