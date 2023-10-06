02f7edd398687d948e7afaf6da5fb0bb01a13ed3
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-05 19:15:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point c program that print information without using
+ * printf, puts or putchar
+ *
+ * Return: 1 (successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
