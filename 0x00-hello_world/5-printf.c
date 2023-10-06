55f417aadd3a1f1e20fa365b997e82222d432f81
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-05 17:31:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of C program that use the printf function
+ * to display information.
+ *
+ * Return: 0 (Sucess).
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
