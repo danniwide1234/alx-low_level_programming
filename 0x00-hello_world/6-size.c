b27bd57d243d37fee7554f7638dd105ea3e3f3bd
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-05 18:36:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,15 +14,15 @@ int main(void)
 	long int p;
 	long long int x;
 	float k;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)
-sizeof(z));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)
-sizeof(y));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)
-sizeof(p));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
-sizeof(x));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)
-sizeof(k));
+printf("Size of a char: %lu byte(s)\n", (unsigne
+d long)sizeof(z));
+printf("Size of a int: %lu byte(s)\n", (unsigned
+long)sizeof(y));
+printf("Size of a long int: %lu byte(s)\n", (uns
+igned long)sizeof(p));
+printf("Size of a long long int: %lu byte(s)\n";
+(unsigned long)sizeof(x));
+printf("Size of a float: %lu byte(s)\n", (unsign
+ed long)sizeof(k));
 return (0);
 }
