0d6475d906994b7c7919f825e22b1ec33894798e
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-06 19:23:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for comipiler to run
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf("%d\n", number);
+	}
+	return (0);
+}
