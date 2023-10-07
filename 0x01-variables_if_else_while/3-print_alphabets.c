deb370c5079b45a93cf4dfead5a16711933cc97d
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-06 18:26:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Compilation entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char smallLetter = 'a';
+	char bigLetter = 'A';
+
+	while (smallLetter <= z)
+	{
+		putchar(smallLetter++);
+	}
+	while (bigLetter <= Z)
+	{
+		putchar(bigLetter++);
+	}
+	putchar('\n');
+	return (0);
+}
