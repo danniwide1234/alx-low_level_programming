5771fa67a0d2e007dff9fa1c48f0e8d8d60fb2f7
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-07 20:41:45 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,38 @@ int main(void)
 {
 	int digit0 = 0;
 	int digit1 = 1;
-	while (digit0 <= 8)
+	int digit2 = 2;
+
+	while (digit0 <= 7)
 	{
-		while (digit <= 9)
+		putchar(digit0 + '0');
+		putchar(digit1 + '0');
+		putchar(digit2 + '0');
+
+		if (digit0 != 7 || digit1 != 8 || digit2 != 9)
 		{
-			putchar(digit0 + '0');
-			putchar(digit1 + '0');
-			putchar((digit0 != 8 || digit1 != 9) ? (',') : ('\n'));
-			digit1++;
+			putchar(',');
+			putchar(' ');
 		}
-		digit0++;
-		digit1 = digit0 + 1;
+
+		digit2++;
+
+		if (digit2 <= 9)
+		{
+			digit1++;
+			digit2 = digit1 + 1;
+		}
+
+		digit1++;
+
+		if (digit1 <= 8)
+		{
+			digit0++;
+			digit1 = digit0 + 1;
+			digit2 = digit1 + 1;
+		}
 	}
+
+	putchar('\n');
 	return (0);
 }
-
