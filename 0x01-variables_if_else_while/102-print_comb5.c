9ee980c90ff54dc92d0ffd9e67f1fc31f93ecd13
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-07 21:36:28 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for compilation
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit0 = 0, digit1 = 1;
+
+	while (digit0 <= 98)
+	{
+		while (digit1 <= 99)
+		{
+			putchar(digit0 / 10 + '0');
+			putchar(digit0 % 10 + '0');
+			putchar(' ');
+			putchar(digit1 / 10 + '0');
+			putchar(digit1 % 10 + '0');
+			putchar((digit0 != 98 || digit1 != 99) ? (',') : ('\n'))
+
+			digit1++;
+		}
+
+		digit0++;
+		digit1 = digit0 + 1;
+	}
+
+	return (0);
+}
