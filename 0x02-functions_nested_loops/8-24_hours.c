363c01d3ba4a2ae531cac5ae2a40f9a343c6a8c8
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-10 18:24:44 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,32 +2,36 @@
 
 /**
  * jack_bauer - prints every minute of the day in 24-hour format.
+ *
+ * Return : ....
  */
+
 void jack_bauer(void)
 {
-	int hour;
-	int minute;
+	int p;
+	int q;
+	int r;
+	int s;
 
-	for (hour = 0; hour < 24; hour++)
+	for (p = 0; p <= 2; p++)
 	{
-		for (minute = 0; minute < 60; minute++)
-		{
-			char time_str[6]; /* "hh:mm\0" */
-
-			time_str[0] = (hour / 10) + '0';
-			time_str[1] = (hour % 10) + '0';
-			time_str[2] = ':';
-			time_str[3] = (minute / 10) + '0';
-			time_str[4] = (minute % 10) + '0';
-			time_str[5] = '\0';
-
-			_putchar(time_str[0]);
-			_putchar(time_str[1]);
-			_putchar(time_str[2]);
-			_putchar(time_str[3]);
-			_putchar(time_str[4]);
-			_putchar(time_str[5]);
-			_putchar('\n');
-		}
+	for (q = 0; q <= 9; q++)
+	{
+	if ((p <= 1 && q <= 9) || (p <= 2 && q <= 3))
+	{
+	for (r = 0; r <= 5; r++)
+	{
+	for (s = 0; s <= 9; s++)
+	{
+	_putchar(p + '0');
+	_putchar(q + '0');
+	_putchar(58);
+	_putchar(r + '0');
+	_putchar(s + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
 	}
 }
