8db130fd1f4b7521859e517ab266c15adcf77dd8
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-10 19:57:34 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,37 @@
  */
 void times_table(void)
 {
-	int k = 0;
+	int k;
 	int m;
-	int product;
+	int n;
+	int o;
+	int p;
 
-	while (k <= 9)
+	for (k = 0; k <= 9; k++)
 	{
-		m = 0;
-		while (m <= 9)
-		{
-			product = k * m;
-
-			if (m > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			if (product < 10)
-				_putchar(' ');
-			else
-				_putchar(product / 10 + '0');
-
-			_putchar(product % 10 + '0');
-			m++;
-		}
-		_putchar('\n');
-		k++;
+	for (m = 0; m <= 9; m++)
+	{
+	n = k * m;
+	if (n > 9)
+	{
+	o = n % 10;
+	p = (n - o) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(p + '0');
+	_putchar(o + '0');
+	}
+	else
+	{
+	if (m != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(n + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
