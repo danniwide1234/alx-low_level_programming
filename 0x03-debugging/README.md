f240ac16e6d71a0950e06657d09e68812402584f
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-12 00:52:13 -0700

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+#0x03. C - Debugging
+This tasks help in having a vivid understanding of what debugging is all about.
+code is bound to encounter issues which must be corrected. Debugging skill is one
+skill every aspiring software engineer must have.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests the positive_or_negative()
+ *function with the case of 0
+ *
+ * @i: The number to be checked.
+ *
+ * Return: No return value (void)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d : %s\n", i, "negative");
+	}
+
+	else if (i > 0)
+	{
+		printf("%d : %s\n", i, "positive");
+	}
+
+	else
+	{
+		printf("%d : %s\n", i, "zero");
+	}
+}
+
