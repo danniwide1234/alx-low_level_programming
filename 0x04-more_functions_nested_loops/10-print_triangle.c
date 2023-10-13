ce747c86cb6af7603ad0a3ae215f00dc138e03eb
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-12 18:48:36 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle then a new line
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int c, f;
+
+		for (c = 1; c < size; c++)
+		{
+			for (f = 1; f < size; f++)
+			{
+				_putchar(' ');
+			}
+			for (f = 1; f <= c; f++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
