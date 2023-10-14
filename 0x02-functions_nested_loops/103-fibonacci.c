f5c9061b335d268a515265e8ea9f8078666f30fc
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-14 16:19:11 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
 
 /**
- * sum_even_fibonacci - Calculate the sum of even Fibonacci
- * terms up to a limit.
+ * main - compilation starting point.
  *
- * @limit: The upper limit for Fibonacci terms
- *
- * Return: The sum of even Fibonacci terms.
- */
-
-unsigned long sum_even_fibonacci(unsigned long limit)
-{
-	unsigned long fibo1 = 1, fibo2 = 2, fibo_next, sum = 0;
-
-	while (fibo2 <= limit)
-	{
-		if (fibo2 % 2 == 0)
-		{
-			sum += fibo2;
-		}
-
-		fibo_next = fibo1 + fibo2;
-		fibo1 = fibo2;
-		fibo2 = fibo_next;
-	}
-
-	return (sum);
-}
-
-/**
- * main - entry point for compiler
- *
- * Return: 0 (success)
+ * Return: 0 (success).
  */
 
 int main(void)
 {
-	unsigned long limit = 4000000;
-	unsigned long result = sum_even_fibonacci(limit);
+	int higestValue = 4000000;
+	int x = 1;
+	int y = 2;
+	int after;
+	int add = 2;
 
-	printf("%lu\n", result);
-
+	while (after <= highestValue)
+	{
+		after = x + y;
+		if (after % 2 == 0)
+			add = add + after;
+		x = y;
+		y = after;
+	}
+	printf("%d\n", add);
 	return (0);
 }
