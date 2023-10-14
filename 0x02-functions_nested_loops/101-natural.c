24d26753f7ba69fd7467eb0b1bf0f0badec93f5d
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-14 15:12:20 -0700

    betty 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for compilation.
+ *
+ * Return: (void)
+ */
+
+int main(void)
+{
+	int add = 0;
+	int y;
+
+	for (y = 1; y <= 10; y++)
+	{
+		if (y % 3 == 0 || y % 5 == 0)
+			add = add + 1;
+	}
+	printf("%d\n", add);
+}
