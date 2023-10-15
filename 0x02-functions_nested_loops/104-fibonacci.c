c557ea41b642ab1c1809db62e8a5da263c9d00b8
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-14 17:39:47 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,43 @@
 
 int main(void)
 {
-	int m;
-	unsigned long int c = 1, d = 2, after;
+	int number;
+	unsigned long fibo1 = 0, fibo2 = 1, add;
+	unsigned long fibo1_less1, fibo2_less1, fibo1_less2, fibo2_less2;
 
-	printf("1, 2");
-
-	for (m = 2; m < 98; m++)
+	for (number = 0; number < 92; number++)
 	{
-		after = c + d;
-		printf(", %lu", after);
+		add = fibo1 + fibo2;
+		printf("%lu, ", add);
+		fibo1 = fibo2;
+		fibo2 = add;
 
-		c = d;
-		d = after;
 	}
+	fibo1_less1 = fibo1 / 10000000000;
+	fibo2_less1 = fibo2 / 10000000000;
+	fibo1_less2 = fibo1 % 10000000000;
+	fibo2_less2 = fibo2 % 10000000000;
+	for (number = 93; number < 99; number++)
+	{
+		unsigned long less1 = fibo1_less1 + fibo2_less1;
+		unsigned long less2 = fibo1_less2 + fibo2_less2;
 
-	printf("\n");
-	return (0);
+		if (fibo1_less2 + fibo2_less2 > 999999999)
+		{
+			less1 += 1;
+			less2 %= 10000000000;
+
+		}
+		printf("%lu%010lu", less1, less2);
+		if (number != 98)
+		{
+			printf(", ");
+		}
+		fibo1_less1 = fibo2_less1;
+		fibo1_less2 = fibo2_less2;
+		fibo2_less1 = less1;
+		fibo2_less2 = less2;
+		}
+		printf("\n");
+		return (0);
 }
