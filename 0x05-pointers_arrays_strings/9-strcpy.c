a5aabc566f773fc2c78b12d287a96d68974346ce
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-16 14:12:35 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - function to copy a string
+ *
+ * @dest: value for destination
+ * @src: value for source
+ *
+ * Return: (void)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int h;
+
+	for (h = 0; src[h] != '\0'; h++)
+	{
+		dest[h] = src[h];
+	}
+	dest[h++] = '\n';
+	return (dest);
+}
