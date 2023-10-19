0059110b9158d28a9c99cf5101643b8c7d16a08f
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-19 13:53:22 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,52 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+ * print_buffer -  function that prints a buffer
+ *
+ * @b: buffer in question
+ * @size: size in view
+ *
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int a = 0;
+	int f;
+	int k;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (a < size)
+	{
+		f = size - a < 10 ? size - a : 10;
+		printf("%08x: ", a);
+		for (k = 0; k < 10; k++)
+		{
+			if (k < f)
+				printf("%02x", *(b + a + k));
+			else
+				printf(" ");
+			if (k % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (k = 0; k < f; k++)
+		{
+			int c = *(b + a + k);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		a += 10;
+	}
+}
