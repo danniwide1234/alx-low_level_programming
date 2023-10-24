fddffdb66ab64fcd3615ad2f69c0ef6f53d5b303
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-24 03:30:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,66 @@
 #include "main.h"
 
 /**
+ * print_comma_and_space - Prints a comma and a space.
+ */
+void print_comma_and_space(void)
+{
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * print_double_space - Prints two spaces.
+ */
+void print_double_space(void)
+{
+	_putchar(' ');
+	_putchar(' ');
+}
+
+/**
  * print_times_table - Prints the n times table, starting with 0.
  * @n: The number for which the times table should be printed.
  */
+
 void print_times_table(int n)
 {
+	int q, r, result;
+
 	if (n > 15 || n < 0)
 		return;
 
-	for (int q = 0; q <= n; q++)
+	for (q = 0; q <= n; q++)
 	{
-		for (int r = 0; r <= n; r++)
+		for (r = 0; r <= n; r++)
 		{
-			int result = q * r;
-			int spaces;
+			result = q * r;
 
-			if (r > 0)
+			if (r == 0)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(result + '0');
 			}
-
-			if (result < 100)
+			else
 			{
-				_putchar(' ');
+				print_comma_and_space();
 				if (result < 10)
 				{
+					print_double_space();
+					_putchar(result + '0');
+				}
+				else if (result < 100)
+				{
 					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
 				}
 				else
 				{
-					spaces = result / 10;
-					_putchar(spaces + '0');
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
 				}
 			}
-
-			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
