ece982d77c22b1baad22b9cdb960b3429c625750
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-10-27 16:03:53 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - starting point to begin the instruction to print name
+ *
+ * @argc: argument count
+ * @argv: argc vector
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+#0x0A. C - argc, argv
+this task help to understand what arguments count and argument vector is all about.
+knowing this concepts help us to tailor our code to behave in certain way
