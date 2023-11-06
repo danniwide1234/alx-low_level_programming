7dfc71d70a157999041f9e8eb24c6956af37a312
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-06 10:32:33 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - function that prints a struct dog
+ *
+ * @d: variable to be worked on
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name : %s\nAge : %f\nOwner : %s\n", d->name, d->age, d->owner);
+}
