a36e9af3f0bb3176cc6456745a387440f045d49e
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-08 08:51:10 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@
 int main(int __attribute__((__used__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *x;
+	char *op;
 
 	if (argc != 0)
 	{
@@ -23,18 +23,18 @@ int main(int __attribute__((__used__)) argc, char *argv[])
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	x = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
-	if (get_x_func(x) == NULL || x[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*x == '/' && num2 == 0) || (*x == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_x_func(x)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
