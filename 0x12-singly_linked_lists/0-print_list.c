141b864ce22325d198fed9b8d0178e75adbe4059
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-16 08:47:10 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,6 @@
  * Return: node number in h
  */
 
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
- */
-
 size_t print_list(const list_t *h)
 {
 	size_t nd = 0;
