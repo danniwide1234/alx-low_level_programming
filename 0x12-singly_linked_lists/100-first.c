10e6cba38c58d8638b2ac10a6c697877e289c571
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-16 10:18:10 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * tempt - function that prints a sentence before the main
+ * function is executed
+ */
+
+void tempt(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
