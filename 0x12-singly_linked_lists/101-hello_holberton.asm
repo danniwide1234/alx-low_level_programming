485466127e0b5e1d642b69997eddd4495a3a0d0e
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-16 10:54:03 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,10 @@
-section .data
-	hello_message db "Hello, Holberton", 0
-
-section .text
-	global main
-
-	extern printf
-
+ global    main
+          extern    printf
 main:
-	push rbp                  ; Save the base pointer
-	mov rdi, hello_message    ; Set the format string
-	call printf               ; Call printf
-	pop rbp                   ; Restore the base pointer
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
+
