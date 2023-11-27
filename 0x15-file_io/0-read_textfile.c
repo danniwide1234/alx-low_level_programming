94a67433f4961b55080963a1ade3a3aac6611446
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-27 10:06:29 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * read_textfile - function that reads a text file
  * and prints it to the POSIX standard output
@@ -6,6 +7,7 @@
  * @letters: count of letters that should be read and printed
  * Return: return actual letters
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_a;
@@ -16,14 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	file_a = open(filename, O_RDONLY);
 	if (file_a == -1)
-	{
-		perror("Error opening the file");
 		return (0);
-	}
 	storage = malloc(sizeof(char) * letters);
 	if (storage == NULL)
 	{
-		perror("Error allocating memory");
 		close(file_a);
 		return (0);
 	}
@@ -31,21 +29,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(file_a);
 	if (p == -1)
 	{
-		perror("Error reading the file");
 		free(storage);
 		return (0);
 	}
 	q = write(STDOUT_FILENO, storage, p);
-	if (q == -1)
-	{
-		perror("Error writing to standard output");
-		free(storage);
-		return (0);
-	}
 	free(storage);
-	if (p != q || p != (ssize_t)letters)
+	if (p != q)
 	{
-		fprintf(stderr, "Error: Incorrect number of letters written\n");
 		return (0);
 	}
 	return (q);
