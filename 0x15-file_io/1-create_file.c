76c382d925f7092876e8ac02bbcbdc726ce9ba41
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2023-11-27 11:35:30 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file_a = open(filename, O_CREAT || O_WRONLY | O_TRUNC, 0600);
+	file_a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_a == -1)
 	{
 		return (-1);
@@ -31,9 +31,8 @@ int create_file(const char *filename, char *text_content)
 		;
 	}
 	command = write(file_a, text_content, alphabet);
-	if (command == -1 || command != alphabet)
+	if (command == -1)
 	{
-		close(file_a);
 		return (-1);
 	}
 	close(file_a);
