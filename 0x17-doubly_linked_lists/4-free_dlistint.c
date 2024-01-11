96645d02b710694d6d356b8f83539d3f89c197e9
danniwide1234 <danniwide.1981@gmail.com>
2024-01-11 10:36:12 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ *
+ * @head: list head
+ * Return: (void)
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *storage;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((storage = head) != NULL)
+	{
+		head = head->next;
+		free(storage);
+	}
+}
